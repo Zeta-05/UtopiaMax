2aec8c130bb2191fec35330caa99f129e049e995
Zeta-05 <vjimmy276@gmail.com>
2024-12-15 12:02:39 +0530

    Update start.py

--- GOKUMUSIC/utils/inline/start.py
+++ GOKUMUSIC/utils/inline/start.py
@@ -10,7 +10,7 @@ def start_panel(_):
             InlineKeyboardButton(
                 text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
             ),
-            InlineKeyboardButton(text=_["S_B_2"], url=f"https://t.me/Anime_Chat_Group_Community"),
+            InlineKeyboardButton(text=_["S_B_2"], url=f"https://t.me/Anime_Chat_Group_Kt"),
         ],
     ]
     return buttons
@@ -26,7 +26,7 @@ def private_panel(_):
         ],
         [
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_6"], url=f"https://t.me/Anime_Chat_Group_Community"),
+            InlineKeyboardButton(text=_["S_B_6"], url=f"https://t.me/Anime_Chat_Group_Kt"),
         ],
         [
             InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper"),
